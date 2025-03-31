b7bc959278ad0fccabba2a7e428d5698da0731c4
Aditya Kumar <adityasrivastava.niet@gmail.com>
2025-03-31 16:52:26 +0530

    resume and navbar update

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,16 +41,16 @@ const Header = () => {
             {menuOpen ? <FaTimes /> : <FaBars />}
           </button>
           <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
-            <li><button onClick={() => scrollToSection('about')}>About</button></li>
-            <li><button onClick={() => scrollToSection('experience')}>Experience</button></li>
-            <li><button onClick={() => scrollToSection('projects')}>Projects</button></li>
-            <li><button onClick={() => scrollToSection('skills')}>Skills</button></li>
-            <li><button onClick={() => scrollToSection('education')}>Education</button></li>
-            <li><button onClick={() => scrollToSection('certifications')}>Certifications</button></li>
-            <li><button onClick={() => scrollToSection('contact')}>Contact</button></li>
+            <li><a href="#about" onClick={() => scrollToSection('about')}>About</a></li>
+            <li><a href="#experience" onClick={() => scrollToSection('experience')}>Experience</a></li>
+            <li><a href="#projects" onClick={() => scrollToSection('projects')}>Projects</a></li>
+            <li><a href="#skills" onClick={() => scrollToSection('skills')}>Skills</a></li>
+            <li><a href="#education" onClick={() => scrollToSection('education')}>Education</a></li>
+            <li><a href="#certifications" onClick={() => scrollToSection('certifications')}>Certifications</a></li>
+            <li><a href="#contact" onClick={() => scrollToSection('contact')}>Contact</a></li>
             <li>
               <a 
-                href="https://drive.google.com/file/d/1gk0vX2q3x4z5G6J7h8g9j0k8l9k9k9k/view?usp=sharing" 
+                href="https://drive.google.com/file/d/1fldTztMS-MU08XEVPBXLoe1RSHx5kZ7T/view?usp=sharing" 
                 download="Resume.pdf" 
                 className="download-resume highlight"
               >
